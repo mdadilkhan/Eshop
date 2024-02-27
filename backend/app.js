fa5dc14c251aca288ce9c58a79cda98fb8c6eb25
Md Adil Khan <md.adil.khan.@longshortlabs.com>
2024-02-27 20:39:43 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,9 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+// https://eshop-tutorial-pyri.vercel.app
 app.use(cors({
-  origin: ['https://eshop-tutorial-pyri.vercel.app',],
+  origin: "http://localhost:3000",
   credentials: true
 }));
 
--- backend/controller/user.js
+++ backend/controller/user.js
@@ -35,7 +35,8 @@ router.post("/create-user", async (req, res, next) => {
 
     const activationToken = createActivationToken(user);
 
-    const activationUrl = `https://eshop-tutorial-pyri.vercel.app/activation/${activationToken}`;
+    // const activationUrl = `https://eshop-tutorial-pyri.vercel.app/activation/${activationToken}`;
+    const activationUrl = `http://localhost:3000/activation/${activationToken}`;
 
     try {
       await sendMail({
@@ -61,6 +62,7 @@ const createActivationToken = (user) => {
     expiresIn: "5m",
   });
 };
+
 
 // activate user
 router.post(
